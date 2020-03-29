2aeadef23f52eb70568c581d990a4441aed8e0bc
somna <free_apples@ymail.com>
2020-03-29 23:58:20 +0700

    design calc

--- src/components/CalcView/CalcView.tsx
+++ src/components/CalcView/CalcView.tsx
@@ -1,35 +1,108 @@
 import React from "react";
 
 export class CalcView extends React.Component<any, any> {
-  value: string = '';
 
-  insert(val: string | number) {
-    this.value += val
+  constructor(props: any) {
+    super(props);
+    this.state = {value: ''};
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClickEqual = this.handleClickEqual.bind(this)
+  }
+
+  insert(val: any) {
+    if (val)
+      this.setState({value: this.state.value + val})
+  }
+
+  equal() {
+
+  }
+
+  handleClick(e: any) {
+    this.insert(e.target.innerText)
+  }
+
+  handleClickEqual(e: any) {
+    this.insert(e.target.innerText)
   }
 
   render() {
     return (
       <table className="CalcView_table">
+        <tbody>
         <tr>
-          <td colSpan={4}><input type="text" id="CalcView_input"/></td>
+          <td colSpan={4}>
+            <input type="text" defaultValue={this.state.value} id="CalcView_input"/>
+          </td>
         </tr>
         <tr>
           <td>
-            <button onClick={this.insert(1)}>1</button>
+            <button onClick={this.handleClick}>C</button>
           </td>
-          <td>2</td>
-          <td>3</td>
+          <td>
+            <button onClick={this.handleClick}>-</button>
+          </td>
+          <td>
+            <button onClick={this.handleClick}>&lt;</button>
+          </td>
+          <td>
+            <button onClick={this.handleClick}>/</button>
+          </td>
         </tr>
         <tr>
-          <td>4</td>
-          <td>5</td>
-          <td>6</td>
+          <td>
+            <button onClick={this.handleClick}>1</button>
+          </td>
+          <td>
+            <button onClick={this.handleClick}>2</button>
+          </td>
+          <td>
+            <button onClick={this.handleClick}>3</button>
+          </td>
+          <td>
+            <button onClick={this.handleClick}>*</button>
+          </td>
         </tr>
         <tr>
-          <td>7</td>
-          <td>8</td>
-          <td>9</td>
+          <td>
+            <button onClick={this.handleClick}>4</button>
+          </td>
+          <td>
+            <button onClick={this.handleClick}>5</button>
+          </td>
+          <td>
+            <button onClick={this.handleClick}>6</button>
+          </td>
+          <td>
+            <button onClick={this.handleClick}>-</button>
+          </td>
         </tr>
+        <tr>
+          <td>
+            <button onClick={this.handleClick}>7</button>
+          </td>
+          <td>
+            <button onClick={this.handleClick}>8</button>
+          </td>
+          <td>
+            <button onClick={this.handleClick}>9</button>
+          </td>
+          <td>
+            <button onClick={this.handleClick}>+</button>
+          </td>
+        </tr>
+        <tr>
+          <td colSpan={2}>
+            <button onClick={this.handleClick}>0</button>
+          </td>
+          <td>
+            <button onClick={this.handleClick}>.</button>
+          </td>
+          <td>
+            <button onClick={this.handleClickEqual}>=</button>
+          </td>
+        </tr>
+        </tbody>
       </table>
     )
   }
