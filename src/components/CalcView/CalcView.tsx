2ca7d0423ebba8966b5f070e5d680000130638aa
somna <free_apples@ymail.com>
2020-04-03 21:13:40 +0700

    styles for calc

--- src/components/CalcView/CalcView.tsx
+++ src/components/CalcView/CalcView.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./CalcView.css"
 
 export class CalcView extends React.Component<any, any> {
 
@@ -6,7 +7,7 @@ export class CalcView extends React.Component<any, any> {
     super(props);
     this.state = {value: ''};
     this.handleClick = this.handleClick.bind(this);
-    this.handleClickEqual = this.handleClickEqual.bind(this)
+    this.handleEqual = this.handleEqual.bind(this)
   }
 
   insert(val: any) {
@@ -14,17 +15,15 @@ export class CalcView extends React.Component<any, any> {
       this.setState({value: this.state.value + val})
   }
 
-  equal() {
-
-  }
 
   handleClick(e: any) {
     this.insert(e.target.innerText)
   }
 
-  handleClickEqual(e: any) {
-    this.insert(e.target.innerText)
+  handleEqual() {
+    this.setState({value: eval(this.state.value)})
   }
+
 
   render() {
     return (
@@ -32,74 +31,74 @@ export class CalcView extends React.Component<any, any> {
         <tbody>
         <tr>
           <td colSpan={4}>
-            <input type="text" defaultValue={this.state.value} id="CalcView_input"/>
+            <input className="CalcView_input" type="text" defaultValue={this.state.value} id="CalcView_input"/>
           </td>
         </tr>
         <tr>
           <td>
-            <button onClick={this.handleClick}>C</button>
+            <button className="CalcView_button" onClick={this.handleClick}>C</button>
           </td>
           <td>
-            <button onClick={this.handleClick}>-</button>
+            <button className="CalcView_button" onClick={this.handleClick}>±</button>
           </td>
           <td>
-            <button onClick={this.handleClick}>&lt;</button>
+            <button className="CalcView_button" onClick={this.handleClick}>&lt;</button>
           </td>
           <td>
-            <button onClick={this.handleClick}>/</button>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <button onClick={this.handleClick}>1</button>
-          </td>
-          <td>
-            <button onClick={this.handleClick}>2</button>
-          </td>
-          <td>
-            <button onClick={this.handleClick}>3</button>
-          </td>
-          <td>
-            <button onClick={this.handleClick}>*</button>
+            <button className="CalcView_button CalcView_button__orange" onClick={this.handleClick}>/</button>
           </td>
         </tr>
         <tr>
           <td>
-            <button onClick={this.handleClick}>4</button>
+            <button className="CalcView_button" onClick={this.handleClick}>1</button>
           </td>
           <td>
-            <button onClick={this.handleClick}>5</button>
+            <button className="CalcView_button" onClick={this.handleClick}>2</button>
           </td>
           <td>
-            <button onClick={this.handleClick}>6</button>
+            <button className="CalcView_button" onClick={this.handleClick}>3</button>
           </td>
           <td>
-            <button onClick={this.handleClick}>-</button>
+            <button className="CalcView_button CalcView_button__orange" onClick={this.handleClick}>*</button>
           </td>
         </tr>
         <tr>
           <td>
-            <button onClick={this.handleClick}>7</button>
+            <button className="CalcView_button" onClick={this.handleClick}>4</button>
           </td>
           <td>
-            <button onClick={this.handleClick}>8</button>
+            <button className="CalcView_button" onClick={this.handleClick}>5</button>
           </td>
           <td>
-            <button onClick={this.handleClick}>9</button>
+            <button className="CalcView_button" onClick={this.handleClick}>6</button>
           </td>
           <td>
-            <button onClick={this.handleClick}>+</button>
+            <button className="CalcView_button CalcView_button__orange" onClick={this.handleClick}>-</button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button className="CalcView_button" onClick={this.handleClick}>7</button>
+          </td>
+          <td>
+            <button className="CalcView_button" onClick={this.handleClick}>8</button>
+          </td>
+          <td>
+            <button className="CalcView_button" onClick={this.handleClick}>9</button>
+          </td>
+          <td>
+            <button className="CalcView_button CalcView_button__orange" onClick={this.handleClick}>+</button>
           </td>
         </tr>
         <tr>
           <td colSpan={2}>
-            <button onClick={this.handleClick}>0</button>
+            <button className="CalcView_button CalcView_button__zero" onClick={this.handleClick}>0</button>
           </td>
           <td>
-            <button onClick={this.handleClick}>.</button>
+            <button className="CalcView_button" onClick={this.handleClick}>.</button>
           </td>
           <td>
-            <button onClick={this.handleClickEqual}>=</button>
+            <button className="CalcView_button CalcView_button__orange" onClick={this.handleEqual}>=</button>
           </td>
         </tr>
         </tbody>
